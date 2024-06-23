a6e1b46d542fb4dce4a70b1e6417f454da09ced7
Kenspectacle <kennyftw2@gmail.com>
2024-06-23 16:57:00 +0200

    renamed: make the file names in the structure more sensical for Calculator

--- src/main/java/com/example/demo/CalculatorController.java
+++ src/main/java/com/example/demo/CalculatorController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HTMLController {
+public class CalculatorController {
 	
 	@GetMapping("/CalcApp")
 	public ModelAndView startCalculator() {
 		
-		ModelAndView calculatorMV = new ModelAndView("CalculatorApplication");
+		ModelAndView calculatorMV = new ModelAndView("CalculatorView");
 		
 		return calculatorMV;
 	}
---
+++ src/main/java/com/example/demo/OperationRESTController.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OperationRESTController {
+	
+	@GetMapping("/calculateJSON")
+	public String calculate(@RequestParam(name = "input") String input) {
+		// TODO: Calculation
+		/*
+		 * - Operation: PLUS, MINUS, MULTIPLY, DIVISION
+		 * - String to Integer => Integer operandAsInt1 = Integer.valueOf(operand1);
+		 * if("PLUS".equals(operation)) {
+		 * 		return int(operand1 + operand2
+		 * }
+		 * 
+		 * if("MINUS".....
+		 * return ....
+		 * 
+		 * 
+		 * return "Calculation Failed: Not recognized Operation";
+		 */
+		Float result = 0.0f;
+		System.out.println(input);
+		return Float.toString(0.0f);
+//		final Float OPERAND1 = Float.valueOf(operand1);
+//		final Float OPERAND2 = Float.valueOf(operand2);
+//
+//		// URL Encodings for Operators
+//		final String PLUS = "+";
+//		final String DIVIDE = "/";
+//		final String MINUS = "-";
+//		final String MULTIPLY = "*";
+//
+//		if (PLUS.equals(operation)) {
+//			result = new Plus().operate(OPERAND1, OPERAND2);
+//		} else if (MINUS.equals(operation)) {
+//			result = new Minus().operate(OPERAND1, OPERAND2);
+//		} else if (DIVIDE.equals(operation)) {
+//			result = new Divide().operate(OPERAND1, OPERAND2);
+//		} else if (MULTIPLY.equals(operation)) {
+//			result = new Multiple().operate(OPERAND1, OPERAND2);
+//		} else {
+//			return "INVALID OPERATION" + operation;
+//		}
+////		operand1 + operation + operand2
+//		return Float.toString(result);
+	}
+}
+
+
+// float numbers verarbeiten 
+// Modulo
+// Jede operation eigene Klasse
+
+//result = Operation.Plus(OPERAND_AS_INT1 + OPERAND_AS_INT2;)
