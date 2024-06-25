931c14917538e2acc9c17891a256f2c5432fe629
Kenspectacle <kennyftw2@gmail.com>
2024-06-25 13:44:06 +0200

    feat: moved the calculator UI to React

--- nexus-frontend/src/App.js
+++ nexus-frontend/src/App.js
@@ -1,24 +1,58 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <input type="text" id="inputField" className="form-control" />
+          <div className="button-row">
+            <button type="button" className="btn btn-primary" onclick="addString('(')">(</button>
+            <button type="button" className="btn btn-primary" onclick="addString(')')">)</button>
+          </div>
+          <div className="button-row">
+              <button type="button" className="btn btn-primary" onclick="addString(1)">1</button>
+              <button type="button" className="btn btn-primary" onclick="addString(2)">2</button>
+              <button type="button" className="btn btn-primary" onclick="addString(3)">3</button>
+            <button type="button" className="btn btn-secondary" onclick="addString('+')">+</button>
+          </div>
+          <div className="button-row">
+              <button type="button" className="btn btn-primary" onclick="addString(4)">4</button>
+              <button type="button" className="btn btn-primary" onclick="addString(5)">5</button>
+              <button type="button" className="btn btn-primary" onclick="addString(6)">6</button>
+            <button type="button" className="btn btn-secondary" onclick="addString('-')">-</button>
+          </div>
+          <div className="button-row">
+              <button type="button" className="btn btn-primary" onclick="addString(7)">7</button>
+              <button type="button" className="btn btn-primary" onclick="addString(8)">8</button>
+              <button type="button" className="btn btn-primary" onclick="addString(9)">9</button>
+            <button type="button" className="btn btn-secondary" onclick="addString('*')">*</button>
+          </div>
+          <div className="button-row">
+              <button type="button" className="btn btn-success" onclick="submitForm()">Submit</button>
+            <button type="button" className="btn btn-primary" onclick="addString(0)">0</button>
+            <button type="reset" className="btn btn-danger" onclick="resetField()">Reset</button>
+            <button type="button" className="btn btn-secondary" onclick="addString('/')">/</button>
+          </div>
+        </div>
+        <div className="col-6">
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">History Calculation</th>
+              </tr>
+            </thead>
+            <tbody id="table-body">
+              <tr>
+                <th>1+1=3</th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+	</div>
   );
 }
 
