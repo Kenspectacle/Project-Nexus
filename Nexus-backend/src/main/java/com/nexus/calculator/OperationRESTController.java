3e572421db559d0c18ee8dada56c6032db2c8319
Kenspectacle <kennyftw2@gmail.com>
2024-06-26 12:21:36 +0200

    feat: now the calculator works in React with Spring Boot backend!

--- Nexus-backend/src/main/java/com/nexus/calculator/OperationRESTController.java
+++ Nexus-backend/src/main/java/com/nexus/calculator/OperationRESTController.java
@@ -1,10 +1,12 @@
 package com.nexus.calculator;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class OperationRESTController {
 	
 	@GetMapping("/calculateJSON")
--- nexus-frontend/src/components/CalculatorButton.js
+++ nexus-frontend/src/components/CalculatorButton.js
@@ -5,10 +5,64 @@ const Button = ({ variant, children }) => {
     const addString = (input) => {
         document.getElementById('inputField').value += input
       };
+
+      // helper function
+    function encodeOperator(operator) {
+    if (operator === "+") {
+        return "%2B"
+    } else if (operator === "/") {
+        return "%2F"
+    } else if (operator === "*") {
+        return "%2A"
+    } else {
+        return operator
+    }
+    }
     
-      const submitForm = () => {
-        console.log('Form Submitted'); 
-      };
+      async function submitForm() {
+        let inputField = document.getElementById("inputField");
+
+        // create fetch url
+        let url = "http://localhost:8080/calculateJSON";
+        console.log(url);
+    
+        // decode input
+        let input = inputField.value;
+        input = input.split("").map(encodeOperator).join("");
+        console.log(input);
+    
+        let queryString = "?input=" + input;
+        console.log(queryString)
+    
+        await fetch(url + queryString)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                console.log('response success');
+                return response.text();
+            })
+            .then(text => {
+                console.log('Response:', text);
+    
+                // Create a new entry in the history table
+                let newTr = document.createElement("tr");
+                let newTh = document.createElement("th");
+                let tableBody = document.getElementById("table-body");
+    
+                newTh.innerHTML = inputField.value + "=" + text;
+                newTr.appendChild(newTh);
+                tableBody.appendChild(newTr);
+    
+                // update input field
+                inputField.value = text;
+    
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error); // Handle errors here
+            });
+    
+    }
     
       const resetField = () => {
         document.getElementById('inputField').value = ''; 
