9937d0422a2d88eef8dc05f7d8498afa3ff821cd
Kenspectacle <kennyftw2@gmail.com>
2024-06-26 14:14:34 +0200

    feat: added a footer

--- nexus-frontend/src/components/CalculatorBody.js
+++ nexus-frontend/src/components/CalculatorBody.js
@@ -2,11 +2,13 @@ import React from "react";
 import CalculatorDisplay from "./CalculatorDisplay";
 import HistoryTable from "./HistoryTable";
 import NavBar from "./NavBar";
+import Footer from "./Footer";
+
 function CalculatorBody() {
   return (
     <>
       <NavBar />
-      <div className="container mt-5">
+      <div className="container mt-5 mb-5">
         <div className="row">
           <div className="col-6">
             <CalculatorDisplay />
@@ -16,6 +18,7 @@ function CalculatorBody() {
           </div>
         </div>
       </div>
+      <Footer />
     </>
   );
 }
