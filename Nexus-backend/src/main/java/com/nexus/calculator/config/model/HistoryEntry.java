3a0de69a246b7802fd00499cd164c0de851c9aae
Kenspectacle <kennyftw2@gmail.com>
2024-06-28 11:09:34 +0200

    feat: added HistoryEntry model

--- Nexus-backend/src/main/java/com/nexus/calculator/config/model/HistoryEntry.java
+++ Nexus-backend/src/main/java/com/nexus/calculator/config/model/HistoryEntry.java
@@ -1,4 +1,25 @@
 package com.nexus.calculator.config.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "HISTORYENTRIES")
 public class HistoryEntry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String expression;
+
+    private String comment;
+
+    public HistoryEntry() {
+
+    }
+
+    public HistoryEntry(String expression, String comment) {
+        this.expression = expression;
+        this.comment = comment;
+    }
 }
