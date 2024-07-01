13c6b54a1dcc7cae6b76c42e19dfaa945f5d6122
Kenspectacle <kennyftw2@gmail.com>
2024-07-01 21:26:25 +0200

    feat: added route to homepage

--- nexus-frontend/src/App.js
+++ nexus-frontend/src/App.js
@@ -4,10 +4,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import CalculatorApp  from "./components/CalculatorApp";
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Homepage />} />
       <Route path="/calcApp" element={<CalculatorApp />} />
     </Routes>
   );
---
+++ nexus-frontend/src/components/Homepage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+
+const Homepage = () => {
+    return(
+        <>
+            <NavBar />
+
+            <Footer />
+        </>
+    )
+};
+
+export default Homepage;
