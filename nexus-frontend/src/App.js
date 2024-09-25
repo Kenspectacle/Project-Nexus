82a992d546b94d6e58be3fe515915bb2d2c17b40
Kenspectacle <kennyftw2@gmail.com>
2024-09-25 11:58:17 +0200

    feat: added a route to login page

--- nexus-frontend/src/App.js
+++ nexus-frontend/src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 import { Route, Routes } from "react-router-dom";
 import CalculatorApp  from "./components/CalculatorApp";
 import Homepage from './components/Homepage';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/calcApp" element={<CalculatorApp />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 }
---
+++ nexus-frontend/src/components/LoginPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+
+const LoginPage = () => {
+    return(
+        <>
+            <NavBar />
+            <Footer />
+        </>
+    )
+};
+
+export default LoginPage;
