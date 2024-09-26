e6536f0d16ea24aa8da064d476d098658f4dd491
Kenspectacle <kennyftw2@gmail.com>
2024-09-26 16:18:53 +0200

    bugfix: fixed the import for homepage and loginpage

--- nexus-frontend/src/App.js
+++ nexus-frontend/src/App.js
@@ -3,9 +3,9 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import { Route, Routes } from "react-router-dom";
-import CalculatorApp  from "./components/CalculatorApp";
-import Homepage from './components/Homepage';
-import LoginPage from './components/LoginPage';
+import CalculatorApp  from "./components/Calculator/CalculatorApp";
+import Homepage from './components/Homepage/Homepage';
+import LoginPage from './components/Homepage/LoginPage';
 
 function App() {
   return (
