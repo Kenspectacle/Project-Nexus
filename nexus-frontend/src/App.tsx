c2c22cb65c23c3b1c0ff07f7776f0d344bfd9dc1
Kenny Gozali <kennyftw2@gmail.com>
2025-08-06 17:23:44 +0200

    refactor: use @ notation for my App file local imports

--- nexus-frontend/src/App.tsx
+++ nexus-frontend/src/App.tsx
@@ -2,11 +2,10 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import { Route, Routes } from "react-router-dom";
-import CalculatorApp  from "./components/Calculator/CalculatorApp.tsx";
-import Homepage from './components/Homepage/Homepage.tsx';
-import LoginPage from './components/LoginPage/LoginPage.tsx';
-import RegisterPage from './components/RegisterPage/RegisterPage.tsx';
-import React from 'react';
+import CalculatorApp  from "@/components/Calculator/CalculatorApp";
+import Homepage from '@/components/Homepage/Homepage';
+import LoginPage from '@/components/LoginPage/LoginPage';
+import RegisterPage from '@/components/RegisterPage/RegisterPage';
 
 function App() {
   return (
